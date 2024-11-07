f5a88ee1f3cde9a2bb0ecd729b0f3b31e5086dd1
XxmartinzuletaxX <mzuleta@gmail.com>
2024-11-06 20:28:28 -0500

    modified:   instance/diary.db 	modified:   main.py 	modified:   templates/login.html

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Card(db.Model):
 
 #Asignación #2. Crear la tabla Usuario
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     login = db.Column(db.String(100), nullable = False )
     password = db.Column(db.String(30), nullable = False)
@@ -51,7 +51,15 @@ def login():
             form_password = request.form['password']
             
             #Asignación #4. Aplicar la autorización
+            users_db = User.query.all()
+
+            for user in users_db:
+                if form_login == user.login and form_password == user.password:
+                    return redirect('/index')
             
+                else:
+                    error = 'Nombre de usuario o contraseña incorrectos'
+                    return render_template('/login.html', error=error)
 
 
             
@@ -67,9 +75,11 @@ def reg():
         password = request.form['password']
         
         #Asignación #3. Hacer que los datos del usuario se registren en la base de datos.
-        
+        user = User(login=login, password=password)
 
-        
+        db.session.add(user)
+        db.session.commit()
+
         return redirect('/')
     
     else:    
